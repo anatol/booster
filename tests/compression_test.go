02afc85dd6d49d5f35fe5940256741d7de97a310
Anatol Pomozov <anatol.pomozov@gmail.com>
2022-02-12 07:58:09 -0800

    Create test for broken lz4 legacy compressor
    Issue #117

--- tests/compression_test.go
+++ tests/compression_test.go
@@ -41,3 +41,17 @@ func TestLz4ImageCompression(t *testing.T) {
 
 	require.NoError(t, vm.ConsoleExpect("Hello, booster!"))
 }
+
+// Tests for https://github.com/anatol/booster/issues/117
+func TestLargeLz4ImageCompression(t *testing.T) {
+	vm, err := buildVmInstance(t, Opts{
+		compression: "lz4",
+		modules:     "nvidia,nvidia_modeset,nvidia_drm,nvidia_uvm,amdgpu",
+		disk:        "assets/ext4.img",
+		kernelArgs:  []string{"root=UUID=5c92fc66-7315-408b-b652-176dc554d370"},
+	})
+	require.NoError(t, err)
+	defer vm.Shutdown()
+
+	require.NoError(t, vm.ConsoleExpect("Hello, booster!"))
+}
