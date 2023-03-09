anatol/booster

commit e13aa77e49aef93cc1370a269785b471758cb546

Replace symlink forest targets with relative path
It makes easier to handle unpacked image files. The symlinks point to
the image's location, rather than to system directories.

generator/generator.go
@@ -243,22 +243,23 @@ func generateInitRamfs(conf *generatorConfig) error {
 // appendCompatibilitySymlinks appends symlinks for compatibility with older firmware that loads extra files from non-standard locations
 func appendCompatibilitySymlinks(img *Image) error {
 	symlinks := []struct{ src, target string }{
-		{"/lib", "/usr/lib"},
-		{"/usr/local/lib", "/usr/lib"},
-		{"/usr/sbin", "/usr/bin"},
-		{"/bin", "/usr/bin"},
-		{"/sbin", "/usr/bin"},
-		{"/usr/local/bin", "/usr/bin"},
-		{"/usr/local/sbin", "/usr/bin"},
-		{"/var/run", "/run"},
-		{"/usr/lib64", "/usr/lib"},
-		{"/lib64", "/usr/lib"},
+		{"/lib", "usr/lib"},
+		{"/usr/local/lib", "../lib"},
+		{"/usr/sbin", "bin"},
+		{"/bin", "usr/bin"},
+		{"/sbin", "usr/bin"},
+		{"/usr/local/bin", "../bin"},
+		{"/usr/local/sbin", "../bin"},
+		{"/var/run", "../run"},
+		{"/usr/lib64", "lib"},
+		{"/lib64", "usr/lib"},
 	}
 
 	for _, l := range symlinks {
 		// Ensure that target always exist which may not be the
 		// case if we only install files from /lib or /bin.
-		if err := img.AppendDirEntry(l.target); err != nil {
+		targetDir := filepath.Dir(filepath.Join(filepath.Dir(l.src), l.target))
+		if err := img.AppendDirEntry(targetDir); err != nil {
 			return err
 		}
 

commit 3f93c9270b9e7f913b3c714c67d8ef765ba67e21

generator: Fix creation of target directory
This attempts to fix a regression introduced in commit
e13aa77e49aef93cc1370a269785b471758cb546 since this commit
the `usr/lib` target directroy is not created if the host system
does not ship any files in `usr/lib` (e.g. Alpine). This causes
initramfs images generated on such systems to not be bootable
as `lib` is a symlink to the non-existend `usr/lib` then.

The problem is the current implementation:

	filepath.Dir(filepath.Join(filepath.Dir(l.src), l.target))

Would return `/usr` for `{"/lib", "usr/lib"}` and hence `/usr/lib`
was never created. I believe this was intended to be:

	filepath.Join(filepath.Dir(l.src), l.target)

This commit changes this accordingly and fixes booting Booster
images on Alpine.

generator/generator.go
@@ -263,7 +263,7 @@ func appendCompatibilitySymlinks(img *Image) error {
 	for _, l := range symlinks {
 		// Ensure that target always exist which may not be the
 		// case if we only install files from /lib or /bin.
-		targetDir := filepath.Dir(filepath.Join(filepath.Dir(l.src), l.target))
+		targetDir := filepath.Join(filepath.Dir(l.src), l.target)
 		if err := img.AppendDirEntry(targetDir); err != nil {
 			return err
 		}