d18f2ec0b4563caf284a2b7c3bbda79b4d712f19
celsius <50845859+c3Ls1US@users.noreply.github.com>
2023-08-08 10:45:46 -0700

    luks: don't wait for usbhid module

--- init/luks.go
+++ init/luks.go
@@ -90,7 +90,6 @@ func recoverClevisPassword(t luks.Token, luksVersion int) ([]byte, error) {
 }
 
 func recoverFido2Password(devName string, credential string, salt string, relyingParty string, pinRequired bool, userPresenceRequired bool, userVerificationRequired bool) ([]byte, error) {
-	usbHidDriverWg.Wait()
 	hidGenericWg.Wait()
 
 	ueventContent, err := os.ReadFile("/sys/class/hidraw/" + devName + "/device/uevent")
